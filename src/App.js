f61845913514d4bb0b9653f505092b28e49fe39f
mk <isafesoft@hotmail.com>
2022-05-25 18:09:08 -0400

    react-redux-project-biolerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     Mark2Win Project: MusicStore_React-Redux-Thunk
+        <div>
+            <img src="img/beemusic-logo.png" alt=""/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
